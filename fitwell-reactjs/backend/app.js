4354a28a3d2a8e91ccb49a8827372b2af742bbb9
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2023-11-14 23:36:55 +0530

    backend setup done

---
+++ fitwell-reactjs/backend/app.js
@@ -0,0 +1,20 @@
+const connectToMongo = require('./db')
+const express = require('express')
+connectToMongo()
+const app = express()
+var cors = require('cors')
+const port = 5001;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.use(cors())
+app.use(express.json())
+ 
+app.get('/', (req, res) => {
+  res.send('hello world') 
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+ 
